48252183e182dbb6f97b1eb3d2a88e263b241efa
ivan <ivan.pavlov.alk@gmail.com>
2021-03-04 22:01:39 +0200

    Task 3 - Completed

---
+++ task_3.py
@@ -0,0 +1,10 @@
+from itertools import zip_longest, islice
+
+names = ['Иван', 'Анастасия', 'Петр', 'Сергей', 'Дмитрий', 'Борис', 'Елена']
+grades = ['9А', '7В', '9Б', '9В', '8Б', '10А', '10Б', '9А']
+res_list = ((name, grade) for (name, grade) in zip_longest(names, grades))
+print(*islice(res_list, max(len(names), len(grades))))
+try:
+    print(next(res_list))
+except StopIteration:
+    print("Generator is empty")
